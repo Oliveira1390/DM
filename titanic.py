a6c0c1eae2f3aa746759a8696d753a12a91e6c63
Camilo <milocosta@yahoo.com.br>
2021-04-07 18:21:49 -0300

    Regressão Linear - Ficou uma zona a visualização

--- titanic.py
+++ titanic.py
@@ -7,39 +7,58 @@ import matplotlib.pyplot as plt
 # %%
 ## Importação da base de dados e criação do Dataframe
 df = pd.read_csv('./titanic/train.csv')
-# %%
 df.head()
 # %%
 ## Atribuindo Entradas e Saídas (X e y)
-X = df.iloc[:,[0,2,3,4,5]]
+X = df.iloc[:,[0,2,4,5]]
+Xmean = X.values ## Transformando em array
 y = df.Survived
-print(X)
-print(y)
+ya = y.values ## Transformando em array
 # %%
 ## Verificação de Missing
 print(f"A entrada X possui missing? {X.isnull().values.any()}")
 print(f"Quantos? {X.isnull().sum().sum()}")
 print(f"Quais colunas possuem missing? \n {X.isnull().sum()}")
 # %%
-## Tratamento do Missing através da exclusão dos itens
-#Xexc = X.dropna()
-#Xexc = Xexc.reset_index(drop=True)
+## Tratamento do Male/Female para 1/0
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+Xmean[:,2] = le.fit_transform(Xmean[:,2])
 # %%
 ## Tratamnto do Missing através da substituição pela Média
 from sklearn.impute import SimpleImputer
 si1 = SimpleImputer(missing_values=np.nan, strategy='mean')
-Xmean = X.values
-si1.fit(Xmean[:,4:])
-Xmean[:,4:] = si1.transform(Xmean[:,4:])
-print(Xmean)
+si1.fit(Xmean[:,3:])
+Xmean[:,3:] = si1.transform(Xmean[:,3:])
 # %%
-## Transformando Saída em Array
-ya = y.values
-# %%
-## Separando em Teste e Treino
+## Separando dados de Teste e Treino
 from sklearn.model_selection import train_test_split
 Xmean_train, Xmean_test, ya_train, ya_test = train_test_split(Xmean, ya, test_size=0.2, random_state=1)
-print(Xmean_train)
-print(Xmean_test)
-print(ya_train)
-print(ya_test)
+# %%
+## Regressão Linear Simples da base de treino
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(Xmean_train,ya_train)
+
+# %%
+## Predizendo os resultados da base de Teste
+ya_pred = reg.predict(Xmean_test)
+# %%
+Xmean_train[:,3]
+# %%
+## Visualizando Resultados de Treino
+plt.scatter(Xmean_train[:,3], ya_train, color='red')
+#plt.plot(Xmean_train[:,3], reg.predict(Xmean_train), color='blue')
+plt.title('Sobreviveram x Idade (Treino)')
+plt.xlabel('Idade')
+plt.ylabel('Sobreviveram')
+plt.show()
+# %%
+## Visualizando Resultados de Testes
+plt.scatter(Xmean_test[:,3], ya_test, color='red')
+#plt.plot(Xmean_train[:,3], reg.predict(Xmean_train), color='blue')
+plt.title('Sobreviveram x Idade (Teste)')
+plt.xlabel('Idade')
+plt.ylabel('Sobreviveram')
+plt.show()
+# %%
